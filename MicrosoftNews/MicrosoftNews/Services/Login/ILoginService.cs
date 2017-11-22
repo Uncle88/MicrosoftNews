210c5b09aa42b9cead7686a99c563f8154de5158
Andrey <andreyproskura88@gmail.com>
2017-11-22 14:50:53 +0200

    view for Login

---
+++ MicrosoftNews/MicrosoftNews/Services/Login/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MicrosoftNews.Services.Login
+{
+    public interface ILoginService
+    {
+       Task Login(string login, string password);
+    }
+}
---
+++ MicrosoftNews/MicrosoftNews/Services/Login/LoginService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MicrosoftNews.Services.Login
+{
+    public class LoginService : ILoginService
+    {
+        public Task Login(string login, string password)
+        {
+            
+        }
+    }
+}
