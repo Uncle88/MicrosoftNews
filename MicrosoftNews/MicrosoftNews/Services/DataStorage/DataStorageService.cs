0ff2ebaf55145775ef1263bbd97b4208235b2d19
Andrey <andreyproskura88@gmail.com>
2017-11-28 15:24:08 +0200

    add SQLite, created DataStorageService

---
+++ MicrosoftNews/MicrosoftNews/Services/DataStorage/DataStorageService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MicrosoftNews.Models;
+
+namespace MicrosoftNews.Services.DataStorage
+{
+    public class DataStorageService : IDataStorageService
+    {
+        public DataStorageService()
+        {
+        }
+
+        public List<NewsItems> GetAllNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NewsItems GetItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MicrosoftNews/MicrosoftNews/Services/DataStorage/IDataStorageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using MicrosoftNews.Models;
+
+namespace MicrosoftNews.Services.DataStorage
+{
+    public interface IDataStorageService
+    {
+        List<NewsItems> GetAllNews();
+        NewsItems GetItem();
+    }
+}
