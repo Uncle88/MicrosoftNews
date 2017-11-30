ce3c2b18cf53e1b356df7bb9cc2c5d998f54351f
Andrey <andreyproskura88@gmail.com>
2017-11-30 16:45:22 +0200

    created service for work with database

---
+++ MicrosoftNews/MicrosoftNews/Services/DatabaseInteraction/DatabaseInteractionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MicrosoftNews.Services.DatabaseInteraction
+{
+    public class DatabaseInteractionService : IDatabaseInteractionService
+    {
+        public DatabaseInteractionService()
+        {
+        }
+
+        public void ReadFromDB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteToDB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MicrosoftNews/MicrosoftNews/Services/DatabaseInteraction/IDatabaseInteractionService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MicrosoftNews.Services.DatabaseInteraction
+{
+    public interface IDatabaseInteractionService
+    {
+        void WriteToDB();
+        void ReadFromDB();
+    }
+}
