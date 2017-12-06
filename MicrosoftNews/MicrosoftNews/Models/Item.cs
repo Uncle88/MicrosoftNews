0181bcbffea33ff642ea48cc1c61f6fbc30befea
Andrey <andreyproskura88@gmail.com>
2017-12-06 19:21:50 +0200

    rest service return ObservableCollection

--- MicrosoftNews/MicrosoftNews/Models/Item.cs
+++ MicrosoftNews/MicrosoftNews/Models/Item.cs
@@ -1,18 +1,30 @@
-﻿using System.Xml.Serialization;
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 using SQLite;
 
 namespace MicrosoftNews.Models
 {
+
     [XmlRoot(ElementName = "item")]
-    public class Item 
+    public class Item
     {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-
-        [XmlElement(ElementName= "title")]
+        [XmlElement(ElementName = "title")]
         public string Title { get; set; }
-
         [XmlElement(ElementName = "description")]
         public string Description { get; set; }
     }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "item")]
+        public List<Item> Item { get; set; }
+    }
+    [XmlRoot(ElementName = "rss")]
+    public class Rss
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+    }
 }
+
--- MicrosoftNews/MicrosoftNews/Services/Rest/RestService.cs
+++ MicrosoftNews/MicrosoftNews/Services/Rest/RestService.cs
@@ -1,64 +1,31 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.IO;
+﻿using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Xml.Linq;
+using System.Xml;
 using System.Xml.Serialization;
 using MicrosoftNews.Models;
-using Newtonsoft.Json;
 
 namespace MicrosoftNews.Services.GettingData
 {
     public class RestService : IRestService
     {
-        //public ObservableCollection<NewsItem> GetData()
-        //{
-        //   var Items = new ObservableCollection<NewsItem>()
-        //    {
-        //        new NewsItem{Text = "New fiches", DescriptionNews = "In Xamarin Forms there are a lot of MVVM frameworks that you can use to develop better code " +
-        //                                                             "based on MVVM pattern, to mention just a few: Prism, MvvmLight, FreshMvvm, MvvmCross, Exrin, etc."},
-        //        new NewsItem{Text = "MicrosoftNews", DescriptionNews = "Reduce risk with multiple layers of security right in the operating system for on-premises and cloud protection."},
-        //        new NewsItem{Text = "Windows", DescriptionNews = "Get a Windows 10 development environment.Start building Universal Windows Platform apps quickly using a virtual machine."},
-        //        new NewsItem{Text = "WinPhone", DescriptionNews = "Lightweight and powerful with unprecedented performance in a laptop this size. Get ideas down fast " +
-        //                                                            "with a backlit keyboard and a display designed for touch and Surface Pen."},
-        //        new NewsItem{Text = "FAQ", DescriptionNews = "A product key is a 25-character code that comes with a Microsoft Office product. " +
-        //                                                     "The product key allows you to install and activate the Office product on your PC."}
-        //    };
-        //    return Items;
-        //}
-
         public async Task<ObservableCollection<Item>> GetData()
         {
+
             ObservableCollection<Item> Items;
             var client = new HttpClient();
-            var response = await client.GetStringAsync("https://news.microsoft.com/feed/");
-
-
-            using (TextReader reader = new StringReader(response))
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Item>));
-                Items = (ObservableCollection<Item>)serializer.Deserialize(reader);
+            var response = await client.GetStreamAsync("https://news.microsoft.com/feed/"); 
+            using (XmlReader reader = XmlReader.Create(response)) 
+            { 
+                XmlSerializer serializer = new XmlSerializer(typeof(Rss), new XmlRootAttribute("rss")); 
+                var result = serializer.Deserialize(reader) as Rss;
+                Items = new ObservableCollection<Item>(result.Channel.Item); 
             }
-
             if (Items == null)
             {
-                Items = new ObservableCollection<Item>();
+                Items = new ObservableCollection<Item>(); 
             }
-
             return Items;
-
-
-
-            //var Items = JsonConvert.DeserializeObject<ObservableCollection<Item>>(response);
-            //return Items;
-
-            //Uri geturi = new Uri("https://news.microsoft.com/feed/"); //replace your xml url  
-            //HttpClient client = new HttpClient();
-            //HttpResponseMessage responseGet = await client.GetAsync(geturi);
-            //string response = await responseGet.Content.ReadAsStringAsync();//Getting response 
-            //var Items = JsonConvert.DeserializeObject<ObservableCollection<NewsItem>>(response);
-            //return Items;
         }
     }
 }
